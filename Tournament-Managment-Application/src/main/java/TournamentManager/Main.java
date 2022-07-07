260e46508464bef6664297ceeb9408f130ef0b8f
Jayden-Etheridge <jayden.etheridge@mthutt.school.nz>
2022-07-07 12:10:55 +1200

    Update file fixes

---
+++ Tournament-Managment-Application/src/main/java/TournamentManager/Main.java
@@ -0,0 +1,19 @@
+package TournamentManager;
+
+/**
+ *
+ * @author jaydenetheridge
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//      Calling on GUI Jframe form class
+        GUI window = new GUI();
+//      Displaying the GUI
+        window.setVisible(true);
+        window.setDefaultCloseOperation(GUI.EXIT_ON_CLOSE);
+        
+        
+    }
+    
+}
